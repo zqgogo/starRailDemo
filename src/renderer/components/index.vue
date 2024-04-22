0b3a5a35d85f8559c6aa0bf2cc096f6a78a88052
lingfeng <474565262@qq.com>
2024-04-22 23:48:29 +0800

    feat: 新版新增mod新增功能

--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -61,7 +61,12 @@
           </div>
           <!-- 角色 -->
           <div class="roleItems">
-            <div class="roleItem" v-for="item in roleList" :key="item.key" @click="setActiveItem(item)">
+            <div
+              class="roleItem"
+              v-for="item in roleList"
+              :key="item.key"
+              @click="setActiveItem(item)"
+            >
               <div class="roleImg" :class="{ fourStar: item.star === 4 }">
                 <img :src="item.head" />
               </div>
@@ -81,15 +86,26 @@
       <div class="modContent" v-if="!!activeKey">
         <div class="typeName">{{ activeItem.name }}</div>
         <div class="modItems">
-          <div class="modItem" v-for="(mod, index) in activeItemMods" :key="index" :class="{active: modIndex === index }" @click="setModActive(index)">
+          <div
+            class="modItem"
+            v-for="(mod, index) in activeItemMods"
+            :key="index"
+            :class="{ active: modIndex === index }"
+            @click="setModActive(index)"
+          >
             <div class="modIcon">
               <div class="point"></div>
             </div>
             <div class="modName" :title="mod.modName">{{ mod.modName }}</div>
-            <i class="el-icon-delete-solid deleteMod" @click.stop="deleteMod(index)"></i>
+            <i
+              class="el-icon-delete-solid deleteMod"
+              @click.stop="deleteMod(index)"
+            ></i>
           </div>
         </div>
-        <div class="noModData" v-if="activeItemMods.length === 0">暂无mod数据</div>
+        <div class="noModData" v-if="activeItemMods.length === 0">
+          暂无mod数据
+        </div>
       </div>
 
       <!-- mod展示區域,按鈕放底部 -->
@@ -97,7 +113,10 @@
         <!-- 滾動播放 -->
         <el-carousel
           height="100%"
-          v-if="!!activeItemMods[modIndex].imgs && activeItemMods[modIndex].imgs.length"
+          v-if="
+            !!activeItemMods[modIndex].imgs &&
+            activeItemMods[modIndex].imgs.length
+          "
         >
           <el-carousel-item
             v-for="(item, imgIndex) in activeItemMods[modIndex].imgs"
@@ -119,6 +138,42 @@
         </div>
       </div>
     </div>
+
+    <el-drawer
+      title="新增mod"
+      :visible.sync="dialogVisible"
+      direction="rtl"
+      :show-close="false"
+      :close-on-press-escape="false"
+      :wrapperClosable="false"
+    >
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="角色" prop="roleKey">
+          <el-select
+            v-model="form.roleKey"
+            placeholder="请选择"
+            @change="roleChange()"
+          >
+            <el-option
+              :label="roleItem.name"
+              :value="roleItem.key"
+              v-for="roleItem in roleList"
+              :key="roleItem.key"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="mod名称" prop="modName">
+          <el-input v-model="form.modName"></el-input>
+        </el-form-item>
+        <el-form-item label="mod路徑">
+          <el-input disabled v-model="form.modPath"></el-input>
+        </el-form-item>
+      </el-form>
+      <span class="drawFooter">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addMod(true)">确 定</el-button>
+      </span>
+    </el-drawer>
   </div>
 </template>
 
@@ -222,6 +277,15 @@ export default {
       configData: null, // 配置文件数据
       activeKey: null, // 当前选中的角色/技能/UI key
       modIndex: -1, // 选择的mod index -1表示未选中
+
+      form: {}, // 弹窗表单
+      rules: {
+        roleKey: [{ required: true, message: "请選擇角色", trigger: "blur" }],
+        modName: [
+          { required: true, message: "请輸入mod名稱", trigger: "blur" },
+        ],
+      },
+      dialogVisible: false, // 弹窗是否显示
     };
   },
 
@@ -262,20 +326,36 @@ export default {
 
     // 选择的角色/技能/场景
     activeItem() {
-      if(!!this.activeKey) {
-        return this.roleList.find(item => {
+      if (!!this.activeKey) {
+        return this.roleList.find((item) => {
           return item.key === this.activeKey;
-        })
+        });
       }
       return null;
     },
 
     // 选择的角色/技能/场景对应的mod列表
     activeItemMods() {
-      if(!!this.activeKey) {
+      if (!!this.activeKey) {
         return this.configData[this.activeKey] || [];
       }
       return [];
+    },
+
+    // 拖拽进来的文件、文件夹路径
+    dropIntoFilePath() {
+      return this.$store.state.dropIntoFilePath || null;
+    },
+  },
+
+  watch: {
+    // 监听拖拽路径变化
+    dropIntoFilePath(url) {
+      let path = decodeURIComponent(url || "");
+      console.log("拖拽进来的文件夹路径:", path);
+      if (!!path) {
+        this.addMod(path);
+      }
     },
   },
 
@@ -309,19 +389,19 @@ export default {
 
     // 选择角色、技能、场景
     setActiveItem(item) {
-      console.log("setActiveItem")
+      console.log("setActiveItem");
       this.activeKey = item.key;
       console.log("activeKey", item);
       console.log("modList", this.configData[item.key]);
       this.modIndex = -1;
-      if(!!this.configData[item.key] && !!this.configData[item.key].length) {
+      if (!!this.configData[item.key] && !!this.configData[item.key].length) {
         this.setModActive(0);
       }
     },
 
     // 选中mod
     setModActive(index) {
-      console.log("setModActive")
+      console.log("setModActive");
       this.modIndex = index;
       let modItem = this.activeItemMods[index];
       if (!!modItem) {
@@ -330,9 +410,12 @@ export default {
         // 获取目录下png、jpg文件
         let imgs = files.filter((fileName) => {
           if (!!fileName) {
-            if(fileName.includes("ShadowRamp")) return false;
+            if (fileName.includes("ShadowRamp")) return false;
             let arr = fileName.toString().split(".");
-            if (arr[arr.length - 1] === "png" || arr[arr.length - 1] === "jpg") {
+            if (
+              arr[arr.length - 1] === "png" ||
+              arr[arr.length - 1] === "jpg"
+            ) {
               return true;
             }
           }
@@ -348,11 +431,20 @@ export default {
               "base64"
             )}`;
           }),
-        }
+        };
         this.$set(this.configData[this.activeKey], this.modIndex, newModItem);
       }
     },
-    
+
+    // 弹窗中选择角色
+    roleChange() {
+      let roleItem = this.roleList.find((i) => i.key === this.form.roleKey);
+      console.log(roleItem);
+      if (!!roleItem) {
+        this.form.roleName = roleItem.name;
+      }
+    },
+
     // 應用mod
     applyMod(isDelete = false) {
       let modInfo = deepClone(this.activeItemMods[this.modIndex]);
@@ -401,12 +493,77 @@ export default {
         return modIndex !== index;
       });
       this.$set(this.configData, this.activeKey, newModList);
-      if(newModList.length === 0) this.modIndex = -1
-      else if(this.modIndex >= index) this.modIndex += -1;
+      if (newModList.length === 0) this.modIndex = -1;
+      else if (this.modIndex >= index) this.modIndex += -1;
       this.updateConfig();
     },
-    
 
+    // mod添加弹窗 || 添加mod || 添加图片
+    addMod(path) {
+      // 彈窗確認
+      if (path === true) {
+        this.$refs.form.validate((valid) => {
+          if (valid) {
+            let configData = deepClone(this.configData);
+            let modList = [
+              ...(configData[this.form.roleKey] || []),
+              deepClone(this.form),
+            ];
+            configData[this.form.roleKey] = modList;
+            this.configData = configData;
+            this.updateConfig();
+
+            if (this.modIndex === -1) this.modChange(0);
+            this.dialogVisible = false;
+            this.$notify({
+              title: "成功",
+              message: "添加成功",
+              type: "success",
+              position: "top-right",
+            });
+          } else {
+            return false;
+          }
+        });
+        return;
+      }
+
+      let newPath = path.toString().split("file:///")[1];
+      this.store.dispatch("changeDropIntoFilePath", null);
+      // 拖拽进来的是图片
+      if (
+        newPath.toString().includes(".png") ||
+        newPath.toString().includes(".jpg")
+      ) {
+        if (!this.activeItem) return;
+        let modItem = this.modList[this.modIndex];
+        let fileName = newPath.split("/").pop();
+        this.fs.copyFileSync(newPath, `${modItem.modPath}/${fileName}`);
+        this.modChange(this.modIndex);
+        return;
+      }
+      // 这里最好检测下拖拽的是不是文件夹
+      // this.readDir(newPath);
+
+      let modName = newPath.split("/");
+      modName = modName[modName.length - 1];
+
+      // 記錄mod信息
+      let detailItem = Object.assign({}, this.activeItem) || {
+        key: "",
+        name: "",
+      };
+      let modItem = {
+        roleKey: detailItem.key,
+        roleName: detailItem.name,
+        modPath: newPath,
+        modName: modName,
+      };
+      console.log(modItem);
+      this.form = modItem;
+
+      this.dialogVisible = true;
+    },
 
     // 檢查文件是否存在
     checkConfig() {
@@ -434,31 +591,31 @@ export default {
       const files = this.fs.readdirSync(path);
       return files;
     },
-    
+
     // 更新config文件
     updateConfig() {
       try {
         // 保存之前删除图片缓存
         let configData = JSON.parse(JSON.stringify(this.configData));
-        Object.keys(configData).forEach(key => {
-          if(!!this.isArray(configData[key]) && !!configData[key].length) {
+        Object.keys(configData).forEach((key) => {
+          if (!!this.isArray(configData[key]) && !!configData[key].length) {
             let roleItemMods = configData[key];
-            roleItemMods.forEach(item => {
+            roleItemMods.forEach((item) => {
               delete item.imgs;
-            })
+            });
           }
-        })
+        });
 
         // 更新文件
         this.fs.writeFileSync(this.filePath, JSON.stringify(configData));
       } catch (error) {
-        console.log("error", error)
+        console.log("error", error);
       }
     },
 
     isArray(obj) {
       return typeof obj === "object" && Array.isArray(obj);
-    }
+    },
   },
 };
 </script>
@@ -608,7 +765,7 @@ export default {
             cursor: pointer;
             border: 2px solid;
 
-            &:hover {    
+            &:hover {
               transform: scale(1.1);
               border-color: white;
             }
@@ -691,8 +848,13 @@ export default {
           cursor: pointer;
           position: relative;
 
-          &:hover, &.active {
-            background: linear-gradient(to right, rgba(255,255,255,0.2), rgba(255,255,255,0));
+          &:hover,
+          &.active {
+            background: linear-gradient(
+              to right,
+              rgba(255, 255, 255, 0.2),
+              rgba(255, 255, 255, 0)
+            );
             .modIcon {
               border-color: rgba(236, 229, 216, 0.6);
 
@@ -712,7 +874,7 @@ export default {
             margin-left: 15px;
             margin-right: 8px;
             transform: rotate(45deg);
-            border: 1.5px solid rgba(236, 229, 216, 0);;
+            border: 1.5px solid rgba(236, 229, 216, 0);
 
             .point {
               width: 10px;
@@ -721,7 +883,6 @@ export default {
               opacity: 0.6;
               margin: 1.5px;
             }
-            
           }
 
           .modName {
@@ -741,7 +902,8 @@ export default {
 
           &.active {
             background: none;
-            .modIcon, .modName {
+            .modIcon,
+            .modName {
               transform: scale(1.1);
               transform-origin: left;
             }
@@ -770,7 +932,7 @@ export default {
     .modDetailContent {
       flex: 1;
       position: relative;
-      background-color: rgba(255,255,255,0.2);
+      background-color: rgba(255, 255, 255, 0.2);
       // border-left: 2px solid #909399;
 
       .noImg {
@@ -815,12 +977,11 @@ export default {
             opacity: 0.2;
           }
 
-          .el-button+.el-button {
+          .el-button + .el-button {
             margin-left: 20px;
           }
         }
       }
-      
 
       &:hover .buttons .el-button {
         opacity: 1;
